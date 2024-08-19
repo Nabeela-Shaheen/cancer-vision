fc2f6e5cebbb13b5132fcaa08b178599e82c1082
Nabeela Shaheen <95758187+Nabeela-Shaheen@users.noreply.github.com>
2024-08-19 13:48:35 +0500

    feat

---
+++ global/constants.js
@@ -0,0 +1,5 @@
+// prod -> https://backend.getweys.net
+// test -> https://getweys-contact-test-api.herokuapp.com
+// dev  -> http://localhost:5000
+
+export const server = 'https://backend.getweys.net';
